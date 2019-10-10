b2eac24b3fcf3f16892bf656ed200f1e817e7543
Brian Bartholomew <me@bcbrian.com>
2019-10-09 21:31:07 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  AUTHENTICATING,
+  UserContext,
+  handleSignInWithEmailLink,
+  onAuthStateChanged
+} from "./firebase/auth";
+
+import Auth from "./routes/Auth";
+import Index from "./routes/Index";
+import DashBoard from "./routes/DashBoard";
+
+import PrivateRoute from "./components/PrivateRoute";
+import AuthRoute from "./components/AuthRoute";
+
+import "./style.css";
+
+function App() {
+  const [user, setUser] = useState(AUTHENTICATING);
+  handleSignInWithEmailLink();
+  useEffect(() => {
+    onAuthStateChanged(user => {
+      return user ? setUser(user) : setUser(null);
+    });
+  }, []);
+  if (user === AUTHENTICATING) {
+    return <div />;
+  }
+  return (
+    <UserContext.Provider value={user}>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/dashboard" component={DashBoard} />
+          <AuthRoute path="/auth" component={Auth} />
+          <Route path="/" exact component={Index} />
+          <Route render={() => <div>404</div>} />
+        </Switch>
+      </Router>
+    </UserContext.Provider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
